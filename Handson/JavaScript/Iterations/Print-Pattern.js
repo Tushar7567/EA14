1029d5419a76e578ce09500a3b61c637fe65f56f
Tushar <107698544+Tushar7567@users.noreply.github.com>
2022-08-06 21:31:59 +0530

    Create Print-Pattern.js

---
+++ Handson/JavaScript/Iterations/Print-Pattern.js
@@ -0,0 +1,14 @@
+
+const Print_pattern = (N) => 
+{
+  for(let a = 0; a < N; a++){
+    let str = "";
+    for(let b = 0; b <= a; b++){
+      str = str + "*"
+      
+    }
+    console.log(str);
+  }
+  
+};
+
