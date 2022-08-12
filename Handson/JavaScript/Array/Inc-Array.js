e2253ea740e804114740add51d75db25accc1c54
Tushar <107698544+Tushar7567@users.noreply.github.com>
2022-08-12 11:58:59 +0530

    Create Inc-Array.js

---
+++ Handson/JavaScript/Array/Inc-Array.js
@@ -0,0 +1,11 @@
+
+const Inc_Arr = (array,N) => 
+{
+  const incArray = array.map(item => item+1);
+  // console.log(incArray);
+  for(const values of incArray){
+    console.log(values);
+  }
+
+};
+ 
