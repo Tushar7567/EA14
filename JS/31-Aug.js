be13291374de883e5f28edf195194a01fad30f72
Tushar <107698544+Tushar7567@users.noreply.github.com>
2022-09-09 15:44:30 +0530

    Create 31-Aug.js

---
+++ JS/31-Aug.js
@@ -0,0 +1,34 @@
+// const handleSubmit = () => {// const response = document.getElementById("name").value;
+// console.log(response);
+// localStorage.setItem("username",response);
+// sessionStorage.setItem("username",response)// }
+// const showDetails = () => {// const data = localStorage.getItem("username");
+// console.log(data);// const data1 = sessionStorage.getItem("username");
+// console.log(data1);// }
+
+
+// // TypeError, ReferenceError, SyntaxErrorfunction 
+//hello(){
+    //console.log("Hello")
+//}
+
+
+//const a = 10;
+// a = 20;
+
+
+// let nameOneOkay;console.log(nameOneOkay);
+
+
+
+// (() => {// some initiation code
+//     const firstVariable = 0;
+//     const secondVariable = 1;
+//     console.log("I am IIFE");
+// })();
+
+// const firstVariable = 1;
+// const secondVariable = 1;
+// console.log(firstVariable, secondVariable);
+
+
